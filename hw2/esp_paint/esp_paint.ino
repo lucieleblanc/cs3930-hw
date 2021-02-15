12763d5b67a1747357e8547374451189f353ba6b
lucieleblanc <lucieleblanc425@gmail.com>
2021-02-15 01:34:06 -0500

    [hw2] start sketch for esp32 components

---
+++ hw2/esp_paint/esp_paint.ino
@@ -0,0 +1,46 @@
+#define drawPin 25
+#define drawLight 0
+#define erasePin 26
+#define eraseLight 2
+#define switchPin 2
+
+int xyzPins[] = { 13, 12, 14 };
+               // VRX VRY SW
+
+void setup() {
+  Serial.begin(115200);
+  // set pinmode for joystick Z-axis button
+  pinMode(xyzPins[2], INPUT_PULLUP);
+  
+  // set pinmode for drawButton
+  // set pinmode for eraseButton
+  // set pinmode for switch
+  
+}
+
+void loop() {
+  // read from each pin, save each value in a var
+  int xVal = analogRead(xyzPins[0]);
+  int yVal = analogRead(xyzPins[1]);
+  int zVal = digitalRead(xyzPins[2]);
+  Serial.printf("X,Y,Z: %d,\t%d,\t%d\n", xVal, yVal, zVal);
+  
+  int drawButtonVal = digitalRead(drawPin);
+  Serial.println(drawButtonVal);
+  // TODO: if draw on, turn on draw light
+
+  int eraseButtonVal = digitalRead(erasePin);
+  Serial.println(eraseButtonVal);
+  // TODO: if erase on, turn on erase light
+
+  int switchVal = digitalRead(switchPin);
+  Serial.println(switchVal);
+
+  // output a value to the Serial console
+  // based on the combination of inputs
+  // not clear how to do this most efficiently
+  // maybe use bitwise ops?
+  
+  // Serial.print('0');
+  delay(100);
+}
