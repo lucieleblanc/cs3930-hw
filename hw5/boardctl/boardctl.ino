a3b793edd0659f41bc7d28257c5316a7b221f0c4
lucieleblanc <lucieleblanc425@gmail.com>
2021-04-16 17:12:55 -0400

    [hw5] separate square types

--- hw5/boardctl/boardctl.ino
+++ hw5/boardctl/boardctl.ino
@@ -1,18 +1,66 @@
-#define PIN_LED 2
-#define PIN_BUTTON 13
+#define BRIDGE_LED 2
+#define BRIDGE_SQUARE 13
+#define TRAM_LED 0
+#define TRAM_SQUARE 12
+
+bool tramLocation;
 
 void setup() {
-    // we'll send signals to the LED
-    pinMode(PIN_LED, OUTPUT);
+    setupBridge();
+    setupTram();
+}
 
-    // we'll receive signals from the button
-    pinMode(PIN_BUTTON, INPUT); 
+void setupBridge() {
+    pinMode(BRIDGE_LED, OUTPUT);
+    pinMode(BRIDGE_SQUARE, INPUT); 
+}
+
+void setupTram() {
+    pinMode(TRAM_LED, OUTPUT);
+    pinMode(TRAM_SQUARE, INPUT); 
+    tramLocation = false;
+    updateTram();
 }
 
 void loop() {
-    if (digitalRead(PIN_BUTTON) == LOW) {
-        digitalWrite(PIN_LED,HIGH); 
+    checkBridge();
+    checkTram();
+}
+
+void checkBridge() {
+    if (digitalRead(BRIDGE_SQUARE) == LOW) {
+        digitalWrite(BRIDGE_LED,HIGH); 
     } else {
-        digitalWrite(PIN_LED,LOW); 
+        digitalWrite(BRIDGE_LED,LOW); 
+    }
+}
+
+void checkTram() {
+    if (digitalRead(TRAM_SQUARE) == LOW) {
+        blinkTramLED();
+        moveTram();
+    } else {
+        digitalWrite(TRAM_LED,LOW);
+    }
+}
+
+void blinkTramLED() {
+    for (int i = 1; i < 5; i++) {
+        digitalWrite(TRAM_LED,HIGH);
+        delay(500);
+        digitalWrite(TRAM_LED,LOW);
+        delay(500);
+    }
+}
+
+void moveTram() {
+    tramLocation = !tramLocation;
+    println("tram is at position " + tramLocation);
+    updateTram();
+}
+
+void updateTram(location) {
+    if (location == true) {
+        
     }
 }
